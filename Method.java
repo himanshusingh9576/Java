6a9678b70b259c7be882ddf87ac12124b90b0d22
Himanshu Kumar Singh <140483011+himanshusingh9576@users.noreply.github.com>
2025-03-08 10:47:35 +0000

    done

--- Method.java
+++ Method.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Method {
-    static int logic(int x, int y){
+    static int logic(int x, int y){     //static method
         int z;
         if(x>y){
             z = x+y;
@@ -15,21 +15,24 @@ public class Method {
         int a = 5;
         int b = 7;
         int c;
-        if(a>b) {
-            c=a+b;
-        }else {
-            c=(a+b)*5;
-            System.out.println(c);
-        } 
+        c = logic(a,b);
+        System.out.println(c);
+        // if(a>b) {
+        //     c=a+b;
+        // }else {
+        //     c=(a+b)*5;
+        
+        // } 
         int a1 = 5;
         int b1 = 6;
         int c1;
-        if(a1>b1) {
-            c1=a1+b1;
-        }else {
-            c1=(a1+b1)*5;
+        c1 = logic(a1,b1);
+        // if(a1>b1) {
+        //     c1=a1+b1;
+        // }else {
+        //     c1=(a1+b1)*5;
             System.out.println(c1);
-        }
+        // }
 
     }
 }
