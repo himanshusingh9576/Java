20ef75f20ba673eb15b2e5fa2913e92fa3f56a1f
Himanshu Kumar Singh <140483011+himanshusingh9576@users.noreply.github.com>
2025-03-09 08:38:45 +0000

    done

---
+++ MethodOverloading.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class MethodOverloading{
+    static void tellJoke(){
+        System.out.println("I invented a new word");
+    }
+    public static void main(String[] args) {
+        tellJoke();
+
+        
+    }
+}
