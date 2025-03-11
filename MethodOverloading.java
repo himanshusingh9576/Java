8c41de3c142f8c1e89329bb9306de65033cbce18
Himanshu Kumar Singh <140483011+himanshusingh9576@users.noreply.github.com>
2025-03-11 13:39:23 +0000

    done

--- MethodOverloading.java
+++ MethodOverloading.java
@@ -9,7 +9,7 @@ public class MethodOverloading{
     }
     static void tellJoke(){
         System.out.println("I invented a new word");
-    }+
+    }
     public static void main(String[] args) {
         tellJoke();
     
