04b76b790f3bc566cdcaabe810a4c7210a02d896
Himanshu Kumar Singh <140483011+himanshusingh9576@users.noreply.github.com>
2025-03-12 05:44:33 +0000

    done

--- MethodOverloading.java
+++ MethodOverloading.java
@@ -1,6 +1,13 @@
 import java.util.Scanner;
 
 public class MethodOverloading{
+    static void foo(){
+        System.out.println("Good Morning bro!");
+    }
+    static void foo(int a) {
+        System.out.println("Good Morning " + a + " bro!");
+    }
+
     static void change(int a){
         a = 98;
     }
@@ -12,14 +19,20 @@ public class MethodOverloading{
     }
     public static void main(String[] args) {
         tellJoke();
-    
-        int x = 45;
-        change(x);
-        System.out.println("The value of x after running change is - " + x);
+        // changing the integer
+        // int x = 45;
+        // change(x);
+        // System.out.println("The value of x after running change is - " + x);
 
+        // changing the array
         int [] marks = {52,76,83,98,81,71};
         change2(marks);
         System.out.println("The value of x after running is: " + marks[0]);
+
+
+        //Method Overloading:- Two or more method can have same name but diffeent parameters. Such methods are called Overloaded Methods
+        foo();
+        // foo(a:3000);
         
 
         
